64cb0ced4e2f4fccb41cf031030c82feac1ee49c
Anuj16091996 <anuj161996@gmail.com>
2021-08-23 23:28:23 -0400

    Python Excercise

---
+++ Assigment1.py
@@ -0,0 +1,16 @@
+staus=True
+
+while staus:
+    print('P - Print numbers')
+    print('A - Add a number')
+    print('M - Display mean of the numbers')
+    print('S - Display the smallest number')
+    print('L - Display the largest number')
+    print('Q - Quit')
+    print();
+    choice=input('Enter your choice:')
+
+    if(choice=='P'):
+            staus=True
+
+

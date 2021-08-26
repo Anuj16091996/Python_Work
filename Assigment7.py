c4d8d8f7cd070a614142cb7421537fb4e144385f
JDDP <2031263@isi-montreal.com>
2021-08-26 18:38:25 -0400

    Added more lines

--- Assigment7.py
+++ Assigment7.py
@@ -1,3 +1,7 @@
 print("Added a demo to check")
 print("Added a demo to check")
 print("Added a demo to check")
+print("Added a demo to check")
+print("Added a demo to check")
+print("Added a demo to check")
+print("Added a demo to check")
